1620a6f5674a5928a8aef8948db5d6fb0c91821d
Cesar Ospino <zeerox23@gmail.com>
2025-01-22 10:05:48 -0500

    corrigiendo la api

--- API/app.py
+++ API/app.py
@@ -13,4 +13,4 @@ def get_familia():
 @app.get("/superheroesDC")
 def get_superheroes():
     rows = ["Superman", "Batman", "Flash", "Linterna Verde", "Mujer maravilla", "Aquaman", "Shazam", "Cyborg"]
-    return rows
+    return rows 
